431e65294d0c646362fddaf608c4378bea13d1ee
Marcus Vinicius da Silveira Richa <marcus.richa@sempreceub.com>
2022-01-24 20:42:47 -0300

    Alterar imagem de employee do app. Alterar db.json com o novo path.

--- pages/employee-of-the-month.js
+++ pages/employee-of-the-month.js
@@ -4,28 +4,35 @@ import Link from 'next/link';
 
 export const EmplyeeOfTheMont = ({ employee }) => {
 	console.log(employee);
-	
+	const { name, position, image, description } = employee.employeeOfTheMonth;
 	
 	return (
 		<div className="page-container">
 			<div>
-				<h1>Emplyee Of The Mont</h1>
+				<h1>Emplyee Of The Month</h1>
 				<Link href="/"><a>Home</a></Link>
+				
+				<h3>{name}</h3>
+				<h6>{position}</h6>
+				<img src={image} />
+				<h5>{description}</h5>
 			</div>
 		</div>
 	);
 };
 
 
+// 1. https://raw.githubusercontent.com/ahoymarcus/nextjs-news-blog/main/db.json
+// 2. https://my-json-server.typicode.com/portexe/next-news/employeeOfTheMonth
 // pageContext is provided automatically by Next-JS
 export const getServerSideProps = async pageContext => {
-	const apiResponse = await fetch('https://my-json-server.typicode.com/portexe/next-news/employeeOfTheMonth');
+	const apiResponse = await fetch('https://raw.githubusercontent.com/ahoymarcus/nextjs-news-blog/main/db.json',);
 	const employee = await apiResponse.json();
 	
-	console.log(employee);
-	
 	return {
-		employee
+		props: {
+			employee
+		}
 	};
 };
 
